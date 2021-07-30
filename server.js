1dd4d29df4bc7c00dabc08175114710c2e6c9ce7
Melvin <melvinrf@Outlook.com>
2021-07-30 08:53:23 -0500

    messing with uri

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const dotenv = require('dotenv');
-const path = require('path');
 
 dotenv.config({path:'config.env'})
 const PORT = 8080;
@@ -17,8 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, {
+mongoose.connect('mongodb+srv://melvin:M0ng0sux123@cluster0.agyb2.mongodb.net/Budget?retryWrites=true&w=majority' || 8080, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
